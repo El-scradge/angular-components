e714310eb5bbd07419f894c8058b4230ca94f401
John Martin <john.martin@fera.co.uk>
2019-10-30 11:24:55 +0000

    shared module updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {DatepickerModule} from './datepicker/datepicker.module';
+import {DragDashboardDirective} from './directives/drag-dashboard.directive';
+import {ModalModule} from './modal/modal.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DragDashboardDirective
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    DatepickerModule,
+    ModalModule
+  ],
+  exports: [
+    DragDashboardDirective
   ]
+
 })
 export class SharedModule { }
